5e63af42c736a1c7c3f79a2ab78fd0f6920585b3
Lucas Cruz <cruz.lucas1@ifsp.edu.br>
2024-11-19 12:26:29 -0300

    initial commit

---
+++ src/main/java/projetos/desafogachefespring/DesafogaChefeSpringApplication.java
@@ -0,0 +1,13 @@
+package projetos.desafogachefespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafogaChefeSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafogaChefeSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/projetos/desafogachefespring/DesafogaChefeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package projetos.desafogachefespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafogaChefeSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
