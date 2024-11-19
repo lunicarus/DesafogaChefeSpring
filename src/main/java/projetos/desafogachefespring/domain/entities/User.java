a68bc4d8b038c494dae2e3fe4588a899dc553207
Lucas Cruz <cruz.lucas1@ifsp.edu.br>
2024-11-19 13:18:25 -0300

    Feat: Creation of User Entity

---
+++ src/main/java/projetos/desafogachefespring/domain/entities/User.java
@@ -0,0 +1,19 @@
+package projetos.desafogachefespring.domain.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String login;
+
+    @Column(nullable = false)
+    private String password;
+
+}
