3cd91e8969733092843b78ba2cf2dd1f88aae889
Lucas Cruz <cruz.lucas1@ifsp.edu.br>
2024-11-19 15:02:38 -0300

    Feat: Creation of WorkSchedule

---
+++ src/main/java/projetos/desafogachefespring/domain/entities/WorkSchedule.java
@@ -0,0 +1,38 @@
+package projetos.desafogachefespring.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalTime;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "work_schedules")
+public class WorkSchedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalTime beginOfShift;
+
+    @Column(nullable = false)
+    private LocalTime endOfShift;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "work_schedule_days", joinColumns = @JoinColumn(name = "work_schedule_id"))
+    @Column(name = "working_day")
+    private Set<String> workingDays;
+
+    public WorkSchedule(LocalTime beginOfShift, LocalTime endOfShift, Set<String> workingDays) {
+        this.beginOfShift = beginOfShift;
+        this.endOfShift = endOfShift;
+        this.workingDays = workingDays;
+    }
+}
