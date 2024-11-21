3719620360296e2dcbcae2a6be7b12d75377bcf1
Lucas Cruz <cruz.lucas1@ifsp.edu.br>
2024-11-21 10:24:21 -0300

    fix: fixed company reference in representant

--- src/main/java/projetos/desafogachefespring/domain/records/RepresentantRecord.java
+++ src/main/java/projetos/desafogachefespring/domain/records/RepresentantRecord.java
@@ -1,24 +1,19 @@
 package projetos.desafogachefespring.domain.records;
 
 
+import projetos.desafogachefespring.domain.entities.Company;
+
 public record RepresentantRecord(long id,
-                                 String login,
-                                 String password,
                                  String name,
-                                 String companyName)
+                                 Company company
+                                 )
 {
     public RepresentantRecord {
-        if (login == null || login.isBlank()) {
-            throw new IllegalArgumentException("Login cannot be null or blank.");
-        }
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("Name cannot be null or blank.");
         }
-        if (password == null || password.isBlank()) {
-            throw new IllegalArgumentException("Password cannot be null or blank.");
-        }
-        if (companyName == null || companyName.isBlank()) {
-            throw new IllegalArgumentException("Company name cannot be null or blank.");
+        if (company == null) {
+            throw new IllegalArgumentException("Company cannot be null or blank.");
         }
     }
 }
